71a706bb809bca24e1570d934673fa8a8ba21002
Dario Balmaceda <dario26081993@uotlook.com.ar>
2022-11-01 10:35:41 -0300

    finished

---
+++ index.js
@@ -0,0 +1,21 @@
+const unReadMessage = document.querySelectorAll(".unread")
+const unread = document.getElementById("notifications")
+const markAll = document.getElementById("mark_all")
+
+unread.innerText = unReadMessage.length
+
+unReadMessage.forEach((message)=>{
+    message.addEventListener("click", ()=>{
+        message.classList.remove("unread");
+        const newUnreadMessage = document.querySelectorAll(".unread")
+        unread.innerText = newUnreadMessage.length
+    })
+})
+
+markAll.addEventListener("click", () =>{
+    unReadMessage.forEach((message) =>{
+        message.classList.remove("unread")
+    })
+    const newUnreadMessage = document.querySelectorAll(".unread")
+    unread.innerText = newUnreadMessage.length
+})
